07cac4a3ae4c3c0f390d592855a13df333b5cede
Michael Zheng <soymikey0717@gmail.com>
2019-02-07 10:11:06 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "space-before-function-paren": 0,
+    indent: 0,
+    quotes: ["error", "double"],
+    semi: [2, "always"]
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import "./cube-ui";
+import App from "./App.vue";
+import "./register";
+import "common/stylus/index.styl";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
