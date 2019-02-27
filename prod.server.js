c2c805187852956fd66b8463c8e3c83e76739492
michael <soymikey0717@gmail.com>
2019-02-27 20:35:13 +0800

    cors

--- prod.server.js
+++ prod.server.js
@@ -7,8 +7,18 @@ const seller = appData.seller;
 const goods = appData.goods;
 const ratings = appData.ratings;
 
+
+
 const router = express.Router();
 
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 router.get("/seller", function(req, res) {
   res.json({
     errno: 0,
@@ -34,7 +44,7 @@ app.use("/api", router);
 
 app.use(express.static("./dist"));
 
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8000;
 
 module.exports = app.listen(port, function(err) {
   if (err) {
