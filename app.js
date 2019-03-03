6dccc0eed962e070584e7e93b8b09f2dda490143
Michael Zheng <soymikey@github.com>
2019-03-03 20:18:29 +0800

    https 8

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const app = express();
 
-var fs = require("fs");
 const appData = require("./data.json");
 const seller = appData.seller;
 const goods = appData.goods;
@@ -10,13 +9,6 @@ const ratings = appData.ratings;
 
 const router = express.Router();
 
-var key = fs.readFileSync("elema-ssl.key");
-var cert = fs.readFileSync("elema-ssl.pem");
-
-var options = {
-  key: key,
-  cert: cert
-};
 router.all("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -52,14 +44,10 @@ app.use(express.static("./dist"));
 
 const port = process.env.PORT || 8000;
 
-// Run static server
-var https = require("https");
-https.createServer(options, app).listen(port);
-
-// module.exports = app.listen(port, function(err) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log("Listening at http://localhost:" + port + "\n");
-// });
+module.exports = app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Listening at http://localhost:" + port + "\n");
+});
